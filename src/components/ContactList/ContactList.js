5dcb62bef09703976aec8e5e424be5e9239f6ed6
Ignacio Iacovino <nacho.iacovino@gmail.com>
2020-02-12 16:48:58 +0100

    Trying to take the functionality out of the ContactList

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,22 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './ContactList.css'
 import NavButtons from '../NavButtons/NavButtons'
 
-const ContactList = () => {
+const ContactList = ({ listContacts, setListContacts }) => {
+    const [currentPage, setCurrentPage] = useState(1)
+    const contactsPerPage = 50
+
+    useEffect(() => {
+        // if (currentPage === 1) setListContacts(listContacts.slice(0, contactsPerPage))
+        // else 
+        setListContacts(listContacts.slice(currentPage * contactsPerPage - contactsPerPage, currentPage * contactsPerPage))
+    }, [currentPage, setListContacts])
+
+    const [totalPages, setTotalPages] = useState(10)
+    // const totalPages = Math.floor(listContacts.length / contactsPerPage)
+
+    useEffect(() => {
+        if (listContacts.length > 0) setTotalPages(Math.floor(listContacts.length / contactsPerPage))
+    }, [listContacts])
+    
     return (
         <div className="ContactList">
             <div className="ContactList-contacts">
-                <div className="ContactList-contact">Tomas Mercado</div>
-                <div className="ContactList-contact">Tomas Mercado</div>
-                <div className="ContactList-contact">Tomas Mercado</div>
-                <div className="ContactList-contact">Tomas Mercado</div>
-                <div className="ContactList-contact">Tomas Mercado</div>
-                <div className="ContactList-contact">Tomas Mercado</div>
-                <div className="ContactList-contact">Tomas Mercado</div>
-                <div className="ContactList-contact">Tomas Mercado</div>
-                <div className="ContactList-contact">Tomas Mercado</div>
+                {listContacts.map(({ id, name }) => 
+                    <div key={id} className="ContactList-contact">{name}</div>
+                )}
             </div>
-            <NavButtons />
+            <NavButtons currentPage={currentPage} setCurrentPage={setCurrentPage} totalPages={totalPages} />
         </div>
     )
 }
--- src/components/LeftColumn/LeftColumn.js
+++ src/components/LeftColumn/LeftColumn.js
@@ -1,18 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SearchBar from '../SearchBar/SearchBar'
 import LetterFilter from '../LetterFilter/LetterFilter'
 import ContactList from '../ContactList/ContactList'
 import './LeftColumn.css'
 
-const LeftColumn = () => {
+const LeftColumn = ({ contacts }) => {
+    const [listContacts, setListContacts] = useState([])
     return (
         <div className="LeftColumn">
             <div className="LeftColumn-search">
                 <SearchBar leftColumn />
             </div>
             <div className="LeftColumn-flex">
-                <LetterFilter />
-                <ContactList />
+                <LetterFilter contacts={contacts} />
+                <ContactList listContacts={listContacts} setListContacts={setListContacts} />
             </div>
         </div>
     )
