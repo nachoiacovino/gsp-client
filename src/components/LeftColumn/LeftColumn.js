553aeff2a5b13faf218f64d64c0c7aae85985d58
Ignacio Iacovino <nacho.iacovino@gmail.com>
2020-02-13 18:35:26 +0100

    LeftColumn search now working

--- src/components/LeftColumn/LeftColumn.js
+++ src/components/LeftColumn/LeftColumn.js
@@ -1,22 +1,36 @@
 import React, { useState, useEffect } from 'react'
+import './LeftColumn.css'
 import SearchBar from '../SearchBar/SearchBar'
 import LetterFilter from '../LetterFilter/LetterFilter'
 import ContactList from '../ContactList/ContactList'
-import './LeftColumn.css'
+import useInputState from '../../hooks/useInputState'
 
 const LeftColumn = ({ contacts, selectContact }) => {
     const [leftContacts, setLeftContacts] = useState([])
+    const [search, setSearch, resetSearch] = useInputState("")
 
     useEffect(() => setLeftContacts(contacts), [contacts])
-
+    
     const filterByLetter = letter => {
-        setLeftContacts(contacts.filter(contact => contact["name"][0] === letter))
+        setLeftContacts(contacts.filter(contact => contact.name[0] === letter))
+    }
+
+    useEffect(() => {
+        setLeftContacts(
+            contacts.filter(contact => contact.name.toLowerCase().includes(search.toLowerCase()))
+        )
+    }, [contacts, search])
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        selectContact(leftContacts[0].id)
+        resetSearch()
     }
 
     return (
         <div className="LeftColumn">
             <div className="LeftColumn-search">
-                <SearchBar leftColumn />
+                <SearchBar leftColumn search={search} setSearch={setSearch} handleSubmit={handleSubmit} />
             </div>
             <div className="LeftColumn-flex">
                 <LetterFilter filterByLetter={filterByLetter} />
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import './SearchBar.css'
-import useInputState from '../../hooks/useInputState'
 import { ReactComponent as SearchIcon } from '../../assets/svg/search.svg'
 
-const SearchBar = ({ leftColumn }) => {
-    const [search, setSearch] = useInputState("")
-
+const SearchBar = ({ leftColumn, search, setSearch, handleSubmit }) => {
     return (
         <div className="SearchBar">
             <SearchIcon className="SearchBar-icon" />
-            <input className={`SearchBar-input ${leftColumn && "leftColumn"}`} type="text" onChange={setSearch} value={search} placeholder="Search..." />
+            <form onSubmit={handleSubmit}>
+                <input className={`SearchBar-input ${leftColumn && "leftColumn"}`} type="text" onChange={setSearch} value={search} placeholder="Search..." />
+            </form>
         </div>
-    );
+    )
 }
 
 export default SearchBar
