022c2532f9784898f87c1ed4e1149f871db5f945
Ignacio Iacovino <nacho.iacovino@gmail.com>
2020-02-13 17:03:00 +0100

    Add selectedContact

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,15 +2,17 @@ import React from 'react'
 import './Header.css'
 import SearchBar from '../SearchBar/SearchBar'
 
-const Header = () => {
+const Header = ({ selectedContact }) => {
     return (
         <div className="Header">
-            <div className="Header-contact">
-                <img className="Header-img" src="https://s3.amazonaws.com/uifaces/faces/twitter/dvdwinden/128.jpg" alt=""/>
-                <h1 className="Header-name">
-                    Verokina O'Gallagher
-                </h1>
-            </div>
+            {selectedContact && 
+                <div className="Header-contact">
+                        <img className="Header-img" src={selectedContact.avatar} alt=""/>
+                        <h1 className="Header-name">
+                            {selectedContact.name}
+                        </h1>
+                </div>
+            }
             <div className="Header-search">
                 <SearchBar />
             </div>
--- src/components/RightColumn/RightColumn.js
+++ src/components/RightColumn/RightColumn.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './RightColumn.css'
 import ContactGrid from '../ContactGrid/ContactGrid'
 import Header from '../Header/Header'
 
 const RightColumn = ({ contacts }) => {
+    const [selectedContact, setSelectedContact] = useState([])
+
+    useEffect(() => {
+        setSelectedContact(contacts[0])
+    }, [contacts])
+    
+    console.log("selected contact", selectedContact)
+    // console.log("contacts right column", contacts)
+
+
     return (
         <div className="RightColumn">
-            <Header />
+            <Header selectedContact={selectedContact} />
             <ContactGrid contacts={contacts} />
         </div>
     )
