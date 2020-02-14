bf4b40bce519929dd998c022c4693854810df094
Ignacio Iacovino <nacho.iacovino@gmail.com>
2020-02-14 13:46:09 +0100

    Delete console.log()

--- src/pages/Contacts/Contacts.js
+++ src/pages/Contacts/Contacts.js
@@ -25,8 +25,6 @@ const Contacts = () => {
         setSelectedContact(contacts.filter(contact => contact.id === id)[0])
     }
 
-    console.log(contacts.slice(0,3))
-
     return (
         <div className="Contacts">
             <LeftColumn contacts={contacts} selectContact={selectContact} />
