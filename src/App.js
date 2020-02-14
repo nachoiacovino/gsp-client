3ad4023c44ff7402ade2d325438015e767bbcd67
Ignacio Iacovino <nacho.iacovino@gmail.com>
2020-02-14 15:34:31 +0100

    Add routing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react'
 import './App.css'
 import Contacts from './pages/Contacts/Contacts'
 import Login from './pages/Login/Login'
+import { Switch, Route } from 'react-router-dom'
 
 const App = () => {
     return (
-        <div>
-            {/* <Contacts /> */}
-            <Login />
-        </div>
+        <Switch>
+            <Route exact path="/" component={Login} />
+            <Route exact path="/contacts" component={Contacts} />
+        </Switch>
     )
 }
 
