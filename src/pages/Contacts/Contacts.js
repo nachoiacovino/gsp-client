ae354c3c0d96e9c41d5600724318187394c50e97
naknekv <nacho.iacovino@gmail.com>
2020-06-16 14:17:14 +0200

    Add conditional for prod

--- src/pages/Contacts/Contacts.js
+++ src/pages/Contacts/Contacts.js
@@ -1,45 +1,61 @@
-import React, { useState, useEffect } from 'react'
-import { withRouter } from 'react-router-dom'
-import axios from 'axios'
-import './Contacts.css'
-import LeftColumn from '../../components/LeftColumn/LeftColumn'
-import RightColumn from '../../components/RightColumn/RightColumn'
+import React, { useState, useEffect } from "react";
+import { withRouter } from "react-router-dom";
+import axios from "axios";
+import "./Contacts.css";
+import LeftColumn from "../../components/LeftColumn/LeftColumn";
+import RightColumn from "../../components/RightColumn/RightColumn";
 
 const Contacts = ({ history }) => {
-    const [isLoggedIn] = useState(sessionStorage.isLoggedIn === "true")
-    const [token] = useState(localStorage.token)
-    const [contacts, setContacts] = useState([])
-    const [selectedContact, setSelectedContact] = useState([])
+  const [isLoggedIn] = useState(sessionStorage.isLoggedIn === "true");
+  const [token] = useState(localStorage.token);
+  const [contacts, setContacts] = useState([]);
+  const [selectedContact, setSelectedContact] = useState([]);
 
-    useEffect(() => {
-        const dynamicSort = property => (a, b) => a[property].localeCompare(b[property])  
-        const fetchData = async () => {
-            const res = await axios.get("http://localhost:3001/contacts", { headers: {"Authorization" : `Bearer ${token}`} })
-            if (res.data.name) history.push("/")
-            else setContacts(res.data.sort(dynamicSort("name")))
-        }
-        fetchData()
-    }, [token, history])
+  useEffect(() => {
+    const dynamicSort = (property) => (a, b) =>
+      a[property].localeCompare(b[property]);
 
-    useEffect(() => {
-        setSelectedContact(contacts[0])
-    }, [contacts])
-
-    const selectContact = id => {
-        setSelectedContact(contacts.filter(contact => contact.id === id)[0])
+    let url;
+    if (process.env.NODE_ENV === "production") {
+      url = "https://gsp-server-3w1sr9578.now.sh/contacts";
+    } else {
+      url = "http://localhost:3001/contacts";
     }
 
-    return (
-        <>
-            {isLoggedIn 
-                ?   <div className="Contacts">
-                        <LeftColumn contacts={contacts} selectContact={selectContact} />
-                        <RightColumn contacts={contacts} selectContact={selectContact} selectedContact={selectedContact} />
-                    </div>
-                :   history.push("/")
-            }
-        </>
-    )
-}
+    const fetchData = async () => {
+      const res = await axios.get(url, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      if (res.data.name) history.push("/");
+      else setContacts(res.data.sort(dynamicSort("name")));
+    };
+    fetchData();
+  }, [token, history]);
 
-export default withRouter(Contacts)
+  useEffect(() => {
+    setSelectedContact(contacts[0]);
+  }, [contacts]);
+
+  const selectContact = (id) => {
+    setSelectedContact(contacts.filter((contact) => contact.id === id)[0]);
+  };
+
+  return (
+    <>
+      {isLoggedIn ? (
+        <div className="Contacts">
+          <LeftColumn contacts={contacts} selectContact={selectContact} />
+          <RightColumn
+            contacts={contacts}
+            selectContact={selectContact}
+            selectedContact={selectedContact}
+          />
+        </div>
+      ) : (
+        history.push("/")
+      )}
+    </>
+  );
+};
+
+export default withRouter(Contacts);
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,80 +1,113 @@
-import React, { useState, useEffect, useRef } from 'react'
-import axios from 'axios'
-import { withRouter } from 'react-router-dom'
-import placeholder from '../../assets/image-placeholder.jpg'
-import './Login.css'
-import useInputState from '../../hooks/useInputState'
-import useToggleState from '../../hooks/useToggleState'
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import { withRouter } from "react-router-dom";
+import placeholder from "../../assets/image-placeholder.jpg";
+import "./Login.css";
+import useInputState from "../../hooks/useInputState";
+import useToggleState from "../../hooks/useToggleState";
 
 const Login = ({ history }) => {
-    const [email, setEmail, resetEmail] = useInputState(localStorage.email || "")
-    const [password, setPassword, resetPassword] = useInputState(localStorage.password || "")
-    const [checkbox, setCheckBox] = useToggleState()
-    const [status, setStatus] = useState("")
-    const emailRef = useRef()
+  const [email, setEmail, resetEmail] = useInputState(localStorage.email || "");
+  const [password, setPassword, resetPassword] = useInputState(
+    localStorage.password || ""
+  );
+  const [checkbox, setCheckBox] = useToggleState();
+  const [status, setStatus] = useState("");
+  const emailRef = useRef();
 
-    useEffect(() => {
-        emailRef.current.focus()
-    }, [])
-    
-    const handleSubmit = e => {
-        e.preventDefault()
-        const handleLogin = async () => {
-            const res = await axios.post("http://localhost:3001/login", { email, password })
-            setStatus(res.data.status)
-            window.localStorage.setItem("token", res.data.token)
-            if (checkbox === true) {
-                localStorage.setItem("email", email)
-                localStorage.setItem("password", password)
-            }
-        }
-        handleLogin()
-        resetEmail()
-        resetPassword()
-        setCheckBox(false)
+  useEffect(() => {
+    emailRef.current.focus();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let url;
+    if (process.env.NODE_ENV === "production") {
+      url = "https://gsp-server-3w1sr9578.now.sh/login";
+    } else {
+      url = "http://localhost:3001/login";
     }
 
-    useEffect(() => {
-        if (status === "success") {
-            sessionStorage.setItem("isLoggedIn", true)
-            history.push("/contacts")
-        }
-        else if (status === "error" || status === "empty") emailRef.current.focus()
-    }, [status, history])
+    const handleLogin = async () => {
+      const res = await axios.post(url, {
+        email,
+        password,
+      });
+      setStatus(res.data.status);
+      window.localStorage.setItem("token", res.data.token);
+      if (checkbox === true) {
+        localStorage.setItem("email", email);
+        localStorage.setItem("password", password);
+      }
+    };
+    handleLogin();
+    resetEmail();
+    resetPassword();
+    setCheckBox(false);
+  };
 
-    const errorOrEmpty = () => {
-        if (status === "error") return "Incorrect username/password. Please try again."
-        else if (status === "empty") return "Please enter email and password."
-        else return
-    }
+  useEffect(() => {
+    if (status === "success") {
+      sessionStorage.setItem("isLoggedIn", true);
+      history.push("/contacts");
+    } else if (status === "error" || status === "empty")
+      emailRef.current.focus();
+  }, [status, history]);
 
-    return (
-        <div className="Login">
-            <div className="Login-image">
-                <img src={placeholder} alt="Placeholder"/>
-            </div>
-            <div className="Login-form">
-                <div className="Login-info info">
-                    Email: contacts@app.com. Password: password. 
-                </div>
-                <div className="Login-title">
-                    <h1>Contacts App</h1>
-                </div>
-                <form onSubmit={handleSubmit}>
-                    <input className="Login-input" type="text" value={email} onChange={setEmail} placeholder="Email Address" aria-label="Email Address" ref={emailRef} />
-                    <input className="Login-input" type="password" value={password} onChange={setPassword} placeholder="Password" aria-label="Password"/>
-                    <label className="Login-checkbox" >
-                        <input type="checkbox" value={checkbox} onChange={setCheckBox} />
-                        Remember me
-                    </label>
-                    <button type="submit">Login</button>
-                </form>
-                <div className={`Login-info ${(status === "error" || status === "empty") && "error"}`}>
-                    {errorOrEmpty()}
-                </div>
-            </div>
+  const errorOrEmpty = () => {
+    if (status === "error")
+      return "Incorrect username/password. Please try again.";
+    else if (status === "empty") return "Please enter email and password.";
+    else return;
+  };
+
+  return (
+    <div className="Login">
+      <div className="Login-image">
+        <img src={placeholder} alt="Placeholder" />
+      </div>
+      <div className="Login-form">
+        <div className="Login-info info">
+          Email: contacts@app.com. Password: password.
         </div>
-    )
-}
+        <div className="Login-title">
+          <h1>Contacts App</h1>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <input
+            className="Login-input"
+            type="text"
+            value={email}
+            onChange={setEmail}
+            placeholder="Email Address"
+            aria-label="Email Address"
+            ref={emailRef}
+          />
+          <input
+            className="Login-input"
+            type="password"
+            value={password}
+            onChange={setPassword}
+            placeholder="Password"
+            aria-label="Password"
+          />
+          <label className="Login-checkbox">
+            <input type="checkbox" value={checkbox} onChange={setCheckBox} />
+            Remember me
+          </label>
+          <button type="submit">Login</button>
+        </form>
+        <div
+          className={`Login-info ${
+            (status === "error" || status === "empty") && "error"
+          }`}
+        >
+          {errorOrEmpty()}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default withRouter(Login)
+export default withRouter(Login);
